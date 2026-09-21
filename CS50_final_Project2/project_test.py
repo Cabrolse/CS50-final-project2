c88073fb6d8138b0fda208afbad7a7382df6f144
Cabrolse <lorbac.erdna@gmail.com>
2026-09-21 17:29:12 +0100

    meow

--- CS50_final_Project2/project_test.py
+++ CS50_final_Project2/project_test.py
@@ -1,7 +1,7 @@
 from project import format_title, format_description, hashtag, save_listing
 
 def title_test():
-    ...
+    assert format_title(x, x, x, x, x) == "x x x's Size x in x conditon"
 def description_test():
     ...
 def hashtag_test():
