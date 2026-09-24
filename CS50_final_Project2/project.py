2eb5e20e30a6bbec5b5d4e02d86dc2d84bae22f3
Cabrolse <lorbac.erdna@gmail.com>
2026-09-24 20:54:06 +0100

    finished comments

--- CS50_final_Project2/project.py
+++ CS50_final_Project2/project.py
@@ -1,29 +1,52 @@
+#used for creating and appending a csv file the vinted listing
 import csv
 
 def main():
-
-    size = input("Size: ").capitalize()
-    style = input("Style (e.x 'Y2K'): ")
-    brand = input("Brand: ").title()
-    condition = input("Condition: ").title()
+    #declare all my fvariables
+    si = empty_input("Size: ")
+    size = si.capitalize()
+    style = empty_input("Style (e.x 'Y2K'): ")
+    bran = empty_input("Brand: ")
+    brand = bran.title()
+    cond = empty_input("Condition: ")
+    condition = cond.title()
+    
+    #checks that price is a digit 
     while True:
         pr = input("Price: ")
         if pr.isdigit():
             break
         print("Inalid input. Only enter numbers please\n")
-    price = pr + ""   
-    category = input("Category(e.x 'T-shirt'): ").title()
+        
+    price = int(pr) + ""   
+    categ = input("Category(e.x 'T-shirt'): ")
+    category = categ.title()
     additional_details = input("Any additional details worth informing buyers:")
+    
+    #call functions
     title = format_title(size, brand, condition, category, style)
     descrpition = format_description(size, brand, condition, category, additional_details, style)
     save_listing(title, descrpition, price)
-
+    
+    
+#checks that each variable is not returned nothing by the user!
+def empty_input(input_text):
+    inp = input(input_text)
+    #tell user inputes text
+    while inp == "":
+        inp = input(input_text)
+    return inp
+    
+#creates a eyecatching informative title of the listing
 def format_title(size: str, brand: str, condition: str, category: str, style: str):
     
-    return f"{brand} {style} {category}'s Size {size} in {condition} conditon"
+    return f"{brand} {style} {category}'s Size {size} in {condition} condition"
 
+#formats a well structured description of listing
 def format_description(size: str, brand: str, condition: str, category: str, additional_details: str, style: str):
+    #generate hashtags
     hashtags = hashtag(size, category, brand, style)
+    # template of description
     descr = (
         f"\n\n"
         f"♦️  Brand: {brand} | Please check measurements in photos for size. Open to offers!\n"
@@ -36,18 +59,23 @@ def format_description(size: str, brand: str, condition: str, category: str, add
     )
     return descr
 
+#generates the hastags for the listing
 def hashtag(size: str, category: str, brand: str, style: str):
-    
+    #eliminates spaces in style and category for hashtag
     hstyle = style.replace(" ", "")
     hcategory = category.replace(" ", "")
-    return f"#{hstyle} #Skerries #Ireland #Dublin #dublinvinted #preloved #vintagestyle #streetweardaily #{brand} #{hcategory} #{size}"
-    
+    #returns all the hashtags
+    return f"#{hstyle} #Skerries #Ireland #Dublin #dublinvinted #preloved #vintagestyle #{brand} #{hcategory} #{size}"
+  
+#saves the listing to a csv file with its own name  
 def save_listing(title: str, description: str, price: int):
     file_name = title.replace(" ", "_") + ".csv"
     
     #create csv file and load data into file
     with open(file_name, mode='a', newline='', encoding='utf-8') as file:
+        #opens file in writer mode
         writer = csv.writer(file)
+        #writes the following variables into the csv file
         writer.writerow([title, price, description])
     print(f"Saved to {file_name}")
     
--- CS50_final_Project2/project_test.py
+++ CS50_final_Project2/project_test.py
@@ -1,10 +1,16 @@
-from project import format_title, format_description, hashtag, save_listing
+from project import format_title, format_description, hashtag
 
-def title_test():
-    assert format_title(x, x, x, x, x) == "x x x's Size x in x conditon"
-def description_test():
-    ...
-def hashtag_test():
-    ...
-def save_listing_test():
-    ...
+def test_title():
+    #corret format
+    assert format_title("x", "x", "x", "x", "x") == "x x x's Size x in x condition"   
+
+def test_description():
+    result = format_description("XL", "Jaded London", "Brand new", "Hoodies", "heel bites", "y2k")
+    #checks for result containing the following 
+    assert "Brand: Jaded London" in result
+    assert "Size: XL" in result
+    assert "#dublinvinted" in result
+    
+def test_hashtag():
+    #ensure all hashtags are included
+    assert hashtag("XS", "pants", "Gucci", "1990s") == "#1990s #Skerries #Ireland #Dublin #dublinvinted #preloved #vintagestyle #Gucci #pants #XS"
